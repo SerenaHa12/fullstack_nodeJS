623b072cf76594a8e68276049a2dc500051c2fa8
SerenaHa12 <serenavander.ha@gmail.com>
2023-08-20 03:06:58 +0700

    new rerpo

---
+++ hoc_jsF8/math/main.js
@@ -0,0 +1,15 @@
+// 1. Toán tử số học - Arithmetic
+// var a = 1;
+// var b = 2;
+// var c = a - b;
+// console.log(c)
+
+// var a = 5;
+// a++;
+// a--;
+// console.log(a);
+
+// 2. Toán tử gán - Assignment
+var a = 1;
+a = a + 2;
+console.log(a)
