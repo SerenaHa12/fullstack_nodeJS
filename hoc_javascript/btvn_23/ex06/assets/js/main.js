7b6b70847176478c448d5d4ee983c71e31980df6
SerenaHa12 <serenavander.ha@gmail.com>
2023-08-23 13:26:06 +0700

    set up

--- hoc_javascript/btvn_23/ex06/assets/js/main.js
+++ hoc_javascript/btvn_23/ex06/assets/js/main.js
@@ -1,4 +1,18 @@
-function showLoginForm() {
-    var form = document.getElementById("loginForm");
-    form.style.display = "block"; // Hiển thị biểu mẫu đăng nhập khi nhấp vào
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var tabHeader = document.querySelector(".tab-header");
+    var tabHeaderElements = tabHeader.querySelectorAll(".tab-header > div");
+    var tabBody = document.querySelector(".tab-body");
+    var tabBodyElements = tabBody.querySelectorAll(".tab-body > div");
+  
+    for (var i = 0; i < tabHeaderElements.length; i++) {
+      (function(index) {
+        tabHeaderElements[index].addEventListener("click", function() {
+          tabHeader.querySelector(".active").classList.remove("active");
+          tabHeaderElements[index].classList.add("active");
+          tabBody.querySelector(".active").classList.remove("active");
+          tabBodyElements[index].classList.add("active");
+        });
+      })(i);
+    }
+  });
+  
