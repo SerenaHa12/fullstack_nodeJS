1cc38ca3fcb2c0f8bfff074db17c533157a845df
SerenaHa12 <serenavander.ha@gmail.com>
2023-08-28 01:31:22 +0700

    JS-Function

--- hoc_jsF8/06.loop/main.js
+++ hoc_jsF8/06.loop/main.js
@@ -11,6 +11,7 @@
         }
 */
 
+// sử dụng hàm console.log để in ra 10 lần dòng chữ Xin chào F8
 // for (var i = 1; i <= 10; i++) {
 //     console.log("Xin chào F8: " + i);
 // }
@@ -27,7 +28,6 @@
 //     console.log(`${i} - ${j}`);
 //   }
 // }
-
 
 // -----------------------------------------------------
 
@@ -102,5 +102,124 @@ Yêu cầu:
 - Số nguyên tố > 1 => <= 1 k phải số nguyên tố
 */
 
-var n = 5;
-if ( n <= 1 || n % 1 !== 0)
+// var n = 5;
+// var check = true; // Giả định n là số nguyên tố
+// if ( n <= 1 || n % 1 !== 0) {
+//     check = false;
+// } else {
+//     for (var i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//             check = false;
+//             // Thoát vòng lặp -> đảm bảo về performance
+//             i = n;
+//         }
+//     }
+// }
+
+// if (check) {
+//     console.log(`${n} là số nguyên tố`)
+// } else {
+//     console.log(`${n} không phải là số nguyên tố`)
+// }
+
+// -----------------------------------------------------
+
+/*
+Bài tập 4: Tạo danh sách số ngẫu nhiên
+
+Để vượt qua thử thách này, bạn cần kết hợp nhiều kiến thức đã học như: 
+    làm việc với array, đối tượng Math và loop.
+
+👉 Hãy tạo hàm getRandNumbers có 3 tham số là min, max, length. 
+Hàm này sẽ trả về một mảng gồm length phần tử, 
+các giá trị trong mảng là số ngẫu nhiên, 
+giá trị trong khoảng từ min tới max.
+
+Gợi ý: 
+Math.random() * (max - min) + min là cách tạo ra 1 số ngẫu nhiên 
+trong khoảng min - max.
+*/
+
+// Làm bài
+
+// function getRandNumbers(min, max, length) {
+//   let arr = [];
+//   for (let i = 0; i < length; i++) {
+//     arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+//   }
+//   return arr;
+// }
+
+/**
+ * Hết sức lưu ý: Hãy suy nghĩ kỹ để đảm bảo vòng lặp (loop)
+ * luôn có điểm dừng, trình duyệt của bạn sẽ bị treo
+ * nếu vòng lặp không có điểm dừng.
+ *
+ * VD 1: for (var i = 0; i < 100; i--) // i++ mới đúng
+ * VD 2: for (var i = 100; i >= 0; i++) // i-- mới đúng
+ * là 2 vòng lặp không có điểm dừng (lặp vô hạn)
+ *
+ * => Treo trình duyệt!!!
+ */
+
+// -----------------------------------------------------
+
+/*
+Bài tập 5: Tính tổng các phần tử là số của mảng
+
+Cho trước mảng numbers, 
+hãy viết hàm getTotal trả về tổng giá trị các phần tử của mảng.
+*/
+// function getTotal(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// Expected results
+// getTotal([1, 2, 3]) // Output: 6
+// getTotal([4, 5, -3]) // Output: 6
+// getTotal([4, 5, 3, 5]) // Output: 17
+// -----------------------------------------------------
+
+/*
+    2. Vòng lặp For/in
+    
+*/
+
+// -----------------------------------------------------
+/*
+    4. Vòng lặp While
+    Cách hoạt động: điều kiện trong while đúng thì chạy, sai thì dừng
+    -> để giải quyết các bài toán không biết trước là lặp lại bao nhiêu lần
+    -> một số bào toán karaoke, tìm chuỗi
+
+    Lưu ý: Tất cả các bài toán sử dụng for đều có thể chuyển thành while 
+    nhưng không có trường hợp ngược lại
+*/
+// var i = 1;
+// while (i <= 10) {
+//     console.log(`Xin chào F8: ${i}`);
+//     i++;
+// }
+
+// var s = 0;
+// var i = 1;
+// while ( i <= 10 ) {
+//     s += i;
+//     i++;
+// }
+// console.log(`S = ${s}`)
+
+/*
+    5. Vòng lặp Do While
+    -> chạy trước kiểm tra sau
+    -> cho dù điều kiện sai vẫn sẽ chạy vòng lặp
+*/
+var i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
---
+++ hoc_jsF8/07.function/main.js
@@ -0,0 +1,15 @@
+/*
+    1. tạo hàm đầu tiên
+
+    - toán tử call() -> toán tử để gọi hảm
+*/
+
+// function showDialog() {
+//     alert(`Hi, xin chao các bẹn`)
+// }
+// showDialog()
+
+function showDialog(mess) { // <- tham số
+    console.log(mess)
+}
+showDialog(`Xin chao các cháu`); // gọi tới function truyền vào giá trị -> đối số
