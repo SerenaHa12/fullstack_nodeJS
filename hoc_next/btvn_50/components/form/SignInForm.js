4a9a9f85067ccba8f8b538d4d16733de5776c4de
SerenaHa12 <serenavander.ha@gmail.com>
2023-11-29 11:36:27 +0700

    day50 btvn emailjs

--- hoc_next/btvn_50/components/form/SignInForm.js
+++ hoc_next/btvn_50/components/form/SignInForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -17,7 +18,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
+import { ToastAction } from "../ui/toast";
+import { useToast } from "../ui/use-toast";
 
 const FormSchema = z.object({
   email: z.string().min(1, {
@@ -29,6 +31,9 @@ const FormSchema = z.object({
 });
 
 const SignInForm = () => {
+  const formSignIn = useRef();
+  const { toast } = useToast();
+
   const form = useForm({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -37,21 +42,40 @@ const SignInForm = () => {
     },
   });
 
-  function onSubmit(data) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-  }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_rt5x9rn",
+        "template_4p3ugch",
+        formSignIn.current,
+        "usf6797X7GiraXuli"
+      )
+      .then(
+        (result) => {
+          toast({
+            title: "Your message has been sent.",
+          });
+          console.log(result.text);
+        },
+        (error) => {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: "There was a problem with your request.",
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+          });
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <div className="container max-w-lg mx-auto p-10 rounded-md">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full mx-auto">
+        <form className="w-full mx-auto" ref={formSignIn} onSubmit={sendEmail}>
+          <h1 className="text-3xl font-semibold mb-4 text-center">Sign In</h1>
           <div className="space-y-2">
             <FormField
               control={form.control}
@@ -61,7 +85,13 @@ const SignInForm = () => {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input type="email" placeholder="Enter your email" {...field} />
+                    <Input
+                      type="email"
+                      name="user_email"
+                      placeholder="Enter your email"
+                      {...field}
+                      required
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -78,7 +108,9 @@ const SignInForm = () => {
                     <Input
                       type="password"
                       placeholder="Enter your password"
+                      name="user_password"
                       {...field}
+                      required
                     />
                   </FormControl>
                   <FormMessage />
@@ -86,15 +118,17 @@ const SignInForm = () => {
               )}
             />
           </div>
-          <Button type="submit" className="mt-4 mx-auto w-full">
-            Sign In
-          </Button>
+          <input
+            type="submit"
+            className="mt-4 mx-auto w-full p-2 rounded-sm border border-solid border-[white] cursor-pointer light:border-[black] light:border light:border-solid"
+            value="Sign In"
+          />
         </form>
         <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
           or
         </div>
         <p className="text-center text-sm text-gray-600 mt-2">
-          If you donnot have an account, please&nbsp;{" "}
+          If you do not have an account, please&nbsp;{" "}
           <Link href="/sign-up" className="text-blue-600 text-bold">
             Sign Up
           </Link>
